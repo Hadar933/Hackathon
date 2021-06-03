ffc855ba1e7a1e9c67333aaeafc065f62b4dd955
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2021-06-03 11:38:53 +0300

    working on plotting RMSE models, as well as committee methods

--- task1/regression.py
+++ task1/regression.py
@@ -1,7 +1,3 @@
-import numpy
-import pickle
-import pandas as pd
-import numpy as np
 ################################################
 #
 #     Task 1 - predict movies revenue & ranking
@@ -9,9 +5,15 @@ import numpy as np
 ################################################
 #
 
-
-from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import numpy
+import pickle
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor, VotingRegressor
 
 
 def RMSE(y, y_hat):
@@ -43,6 +45,14 @@ def get_Xy1y2_from_pickle(p_file_name):
     return df, revenue, vote_avg
 
 
+def plot_many_models(df, y, regressor_lst):
+    for model in regressor_lst:
+        print(f"current model = {model}")
+        rmse_plot(df, y, model)
+    plt.legend(regressor_lst)
+    plt.show()
+
+
 def predict(csv_file):
     """
     This function predicts revenues and votes of movies given a csv file with movie details.
@@ -54,6 +64,52 @@ def predict(csv_file):
     # your code goes here...
 
     pass
+
+
+def rmse_plot(df, y, model):
+    """
+    plots RMSE error of train and test data for some given model
+    :param df: dataframe
+    :param y: labels
+    :param model: the model in hand
+    """
+    train, test, y_train, y_test = train_test_split(df, y)  # default is train = 75%, test=25%
+    train_size = train.shape[0]
+    test_rmse_err = []
+    average_y_hat = []  # average
+    min_index = 2  # for data < 100, 1% (for example) isn't even one sample, so we give a threshold
+    count = 0
+    for p in range(min_index, 101):
+        count += 1
+        # sliced data given some percentage:
+        max_index = int((p / 100) * train_size)
+        curr_training_set = train.iloc[:max_index, :]
+        curr_test_set = test.iloc[:max_index, :]
+        curr_y_train = y_train[:max_index]
+        curr_y_test = y_test[:max_index]
+
+        # fitting on train, predicting on test
+        model.fit(curr_training_set, curr_y_train)
+        y_hat = model.predict(curr_test_set)
+
+        # calculating error
+        curr_test_rmse_err = RMSE(curr_y_test, y_hat)
+        test_rmse_err.append(curr_test_rmse_err)
+
+    plt.title("RMSE values as function of p%")
+    plt.xlabel('Percentage (p%)'), plt.ylabel('RMSE')
+    percentage = range(min_index, 101)
+    plt.plot(percentage, test_rmse_err)
+
+
+def committee(models):
+    """
+    generates a learner using committee method
+    :param models:
+    :return: comittee model (untrained)
+    """
+    estimators = [(str(model)[:-2], model) for model in models]  # creating estimators input syntax
+    return VotingRegressor(estimators=estimators)
 
 
 def split():
@@ -72,4 +128,11 @@ def split():
 
 
 if __name__ == '__main__':
-    X, revenue, vote_avg = get_Xy1y2_from_pickle("trash.pkl")
+    X, y1, y2 = get_Xy1y2_from_pickle("trash.pkl")
+    X = X[['runtime', 'vote_count', 'budget']]
+    gbr = GradientBoostingRegressor()
+    rfr = RandomForestRegressor()
+    lr = LinearRegression()
+    com = committee([gbr, rfr, lr])
+    plot_many_models(X, y1, [gbr, rfr, lr, com])
+
