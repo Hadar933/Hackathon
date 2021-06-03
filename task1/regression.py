0eee682e18ad45ef68ee009cbd0f265a4f1e2612
Hadar Sharvit <hadar.sharvit1@mail.huji.ac.il>
2021-06-03 18:43:37 +0300

    fixed the error plots

--- task1/regression.py
+++ task1/regression.py
@@ -13,6 +13,7 @@ from sklearn.model_selection import train_test_split, KFold
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor, VotingRegressor
+from sklearn.metrics import mean_squared_error
 
 
 def predict(csv_file):
@@ -35,7 +36,7 @@ def RMSE(y, y_hat):
     :param y_hat: prediction
     :return: rmse
     """
-    return np.sqrt(np.mean((y_hat - y) ** 2))
+    return (mean_squared_error(y, y_hat))
 
 
 def get_Xy1y2_from_pickle(p_file_name, which_label):
@@ -70,59 +71,32 @@ def test_model_error(df, y, model):
     :param y: labels
     :param model: the model in hand
     """
-    train, test, y_train, y_test = train_test_split(df, y)  # default is train = 75%, test=25%
-    train_size = train.shape[0]
-    test_rmse_err, train_rmse_err = [], []
-    min_index = 1  # for data < 100, 1% (for example) isn't even one sample, so we give a threshold
-    for p in range(min_index, 101):
-        # sliced data given some percentage:
-        test_set, train_set, curr_y_test, curr_y_train = slice_data(p, test, train, train_size, y_test, y_train)
-        # fitting on train, predicting on test:
-        y_hat_test, y_hat_train = fit_and_predict(test_set, train_set, curr_y_train, model)
-        # calculating error:
-        calculate_error(curr_y_test, curr_y_train, test_rmse_err, train_rmse_err, y_hat_test, y_hat_train)
-    plot_rmse(min_index, model, test_rmse_err, train_rmse_err)
+    print(f"Current model is {model}")
+    X_train, X_test, y_train, y_test = train_test_split(df, y)  # default is train = 75%, test=25%
+    train_size = X_train.shape[0]
+    test_mse_err, train_mse_err = [], []
+    for i in range(1, train_size):
+        model.fit(X_train.iloc[:i, :], y_train.iloc[:i])
+        y_hat_train = model.predict(X_train.iloc[:i, :])
+        y_hat_test = model.predict(X_test)
+        test_mse_err.append(mean_squared_error(y_test, y_hat_test))
+        train_mse_err.append(mean_squared_error(y_train.iloc[:i], y_hat_train))
+    plot_rmse(model, test_mse_err, train_mse_err)
 
 
-def plot_rmse(min_index, model, test_rmse_err, train_rmse_err):
+def plot_rmse(model, test_mse_err, train_mse_err):
     """
     plots RMSE error for some given model
-    :param min_index:
     :param model:
-    :param test_rmse_err:
-    :param train_rmse_err:
-    :return:
+    :param test_mse_err:
+    :param train_mse_err:
     """
-    plt.title(f"{model} RMSE values as function of p%")
-    plt.xlabel('Percentage (p%)'), plt.ylabel('RMSE')
-    percentage = range(min_index, 101)
-    plt.plot(percentage, test_rmse_err)
-    plt.plot(percentage, train_rmse_err)
+    plt.title(f"{re.split(model,)} RMSE values as function of p%")
+    plt.xlabel('# samples'), plt.ylabel('RMSE')
+    plt.plot(np.sqrt(test_mse_err))
+    plt.plot(np.sqrt(train_mse_err))
     plt.legend(["Test", "Train"])
     plt.show()
-
-
-def calculate_error(curr_y_test, curr_y_train, test_rmse_err, train_rmse_err, y_hat_test, y_hat_train):
-    curr_test_rmse_err = RMSE(curr_y_test, y_hat_test)
-    curr_train_rmse_err = RMSE(curr_y_train, y_hat_train)
-    test_rmse_err.append(curr_test_rmse_err)
-    train_rmse_err.append(curr_train_rmse_err)
-
-
-def fit_and_predict(curr_test_set, curr_training_set, curr_y_train, model):
-    model.fit(curr_training_set, curr_y_train)
-    y_hat_test = model.predict(curr_test_set)
-    y_hat_train = model.predict(curr_training_set)
-    return y_hat_test, y_hat_train
-
-
-def slice_data(p, test, train, train_size, y_test, y_train):
-    max_index = int((p / 100) * train_size)
-    curr_training_set = train.iloc[:max_index, :]
-    curr_test_set = test.iloc[:max_index, :]
-    curr_y_train = y_train[:max_index]
-    curr_y_test = y_test[:max_index]
-    return curr_test_set, curr_training_set, curr_y_test, curr_y_train
 
 
 def committee(models):
@@ -175,6 +149,6 @@ if __name__ == '__main__':
     lasso = Lasso(tol=0.001)
     ridge = Ridge(normalize=True)
     com = committee([lasso, ridge, rfr, lr])
-    all = [lr]
+    all = [lr, rfr, lasso, ridge, com]
     for m in all:
         test_model_error(X, y, m)
