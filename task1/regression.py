2088b30874d7c1a977eb4bbf12ddf7e2b78955a2
nirgaavrielov <nir.gavrielov@mail.huji.ac.il>
2021-06-03 09:16:15 +0300

    spliting

--- task1/regression.py
+++ task1/regression.py
@@ -1,10 +1,15 @@
+import numpy
 
+import pandas as pd
+from sklearn import model_selection
+import numpy as np
 ################################################
 #
 #     Task 1 - predict movies revenue & ranking
 #
 ################################################
 #
+
 
 def predict(csv_file):
     """
@@ -17,3 +22,19 @@ def predict(csv_file):
     #your code goes here...
 
     pass
+
+
+def split():
+    df = pd.read_csv('movies_dataset.csv')
+    train, validate, test = \
+        np.split(df.sample(frac=1, random_state=42),
+                 [int(.6 * len(df)), int(.8 * len(df))])
+    trash_idx = np.zeros((train.shape[0])).astype(numpy.bool)
+    trash_idx[[np.random.randint(0, train.shape[0], 100)]] = 1
+    trash = train.iloc[trash_idx]
+    train = train.iloc[~trash_idx]
+    train.to_pickle('train.pkl')
+    test.to_pickle('test.pkl')
+    validate.to_pickle('valid.pkl')
+    trash.to_pickle('trash.pkl')
+
